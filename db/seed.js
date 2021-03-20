068be3da3c40bbf75f54591e9fdef250b3c1e1b5
Matthew Turner <turnermb92@gmail.com>
2021-03-20 10:59:35 -0500

    first commit

---
+++ db/seed.js
@@ -0,0 +1,18 @@
+// Import the database connection
+const mongoose = require("./connection");
+
+//////////////////////////////////
+// Import Models
+/////////////////////////////////
+
+/////////////////////////////////
+// Do your Database Operations in Below Function
+/////////////////////////////////
+const seed = async () => {
+  //--- CODE GOES HERE
+  console.log("seed file");
+  //--------------------
+};
+
+// run seed function
+seed();
